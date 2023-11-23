5ff25367733f08e171a62366c324d47af6a548cb
James Cole <james@firefly-iii.org>
2023-11-23 08:28:04 +0100

    Update run.php

--- run.php
+++ run.php
@@ -65,7 +65,7 @@ if (!file_exists($destination)) {
 
     foreach ($currencies as $from) {
         $log->debug(sprintf('Will now query rates of currency "%s"', $from));
-        $url  = sprintf('https://api.exchangerate.host/latest?base=%s&symbols=%s&access_key=%s', $from, join(',', $currencies), $_ENV['EXCHANGE_RATE_API_KEY']);
+        $url  = sprintf('https://api.exchangerate.host/latest?base=%s&symbols=%s&access_key=%s', $from, join(',', $currencies), $_ENV['accessKey']);
         $json = download($log, $url);
 
         foreach ($json['rates'] as $to => $rate) {
